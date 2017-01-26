34c1795a5060d6f3fd105afb60c9362668de7738
belikovanastasya <belikovaava@gmail.com>
2017-01-26 14:15:42 +0200

    Contact, our team and letternews blocks layout

---
+++ js/script.js
@@ -0,0 +1,53 @@
+$('#indicatorContainer-1').radialIndicator({
+        barColor: '#e74c3c',
+        barWidth:6,
+        initValue: 0,
+        roundCorner : true,
+        percentage: true
+    });
+
+var radialObj = $('#indicatorContainer-1').data('radialIndicator');
+//now you can use instance to call different method on the radial progress.
+//like
+radialObj.animate(97);
+
+
+$('#indicatorContainer-2').radialIndicator({
+        barColor: '#e74c3c',
+        barWidth: 6,
+        initValue: 0,
+        roundCorner : true,
+        percentage: true
+    });
+
+var radialObj = $('#indicatorContainer-2').data('radialIndicator');
+//now you can use instance to call different method on the radial progress.
+//like
+radialObj.animate(98);
+
+$('#indicatorContainer-3').radialIndicator({
+        barColor: '#e74c3c',
+        barWidth: 6,
+        initValue: 0,
+        roundCorner : true,
+        percentage: true
+    });
+
+var radialObj = $('#indicatorContainer-3').data('radialIndicator');
+//now you can use instance to call different method on the radial progress.
+//like
+radialObj.animate(93);
+
+
+$('#indicatorContainer-4').radialIndicator({
+        barColor: '#e74c3c',
+        barWidth: 6,
+        initValue: 0,
+        roundCorner : true,
+        percentage: true
+    });
+
+var radialObj = $('#indicatorContainer-4').data('radialIndicator');
+//now you can use instance to call different method on the radial progress.
+//like
+radialObj.animate(89);
